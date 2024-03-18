d9e0e022d43fd29746431c4fa803bbf0ececdec0
Diego Lerma <dieglt_98@hotmail.com>
2024-03-17 23:02:23 -0600

    Inicio proyecto

--- online.py
+++
@@ -1,8 +0,0 @@
-def online_counter(dict):
-    online_users_counter=0
-    for status in dict.values():
-        if status=='online':
-            online_users_counter+=1
-    return online_users_counter
-
-print(online_counter({'user1': 'online', 'user2': 'offline', 'user3': 'offline', 'user4': 'online'}))
